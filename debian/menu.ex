952b3963a48910a15fdf12c6a80a0adaf8f14939
Jack Christensen <jack@jackchristensen.com>
2014-06-07 19:01:16 -0500

    Enter necessary package config

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(tpr):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="tpr" command="/usr/bin/tpr"
--- debian/tpr.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the tpr package
-#
-0 4	* * *	root	[ -x /usr/bin/tpr_maintenance ] && /usr/bin/tpr_maintenance
